466af5ae42de2d535fac631a34ef0e447a31cbc3
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2021-01-05 15:53:52 -0500

    Fixed linkage issue with Sandbox.h

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,204 +1,250 @@
-#include "SandboxApp.h"
+///////////////////////////////////////////////////////////////////////////////////////////////
+// 1. #include "Entropy.h"                                                                   //
+// 2. Create a sub class that inherits from Entropy::Application                             //
+// 3. Call Entropy::CreateApplication() returns your class instance                          //
+// 4. Implement OnUpdate() and OnCreate() pure virtual methods                               //
+// You're set!                                                                               //
+///////////////////////////////////////////////////////////////////////////////////////////////
 
-void SandboxGame::OnCreate()
+#include "Entropy.h"
+
+class SandboxGame : public Entropy::Application
 {
-	m_CameraController.GetCamera().SetPosition({ 0.0f, 1.0f, -8.0f });
-
-	m_DefaultShader = m_ShaderLibrary.Load("./assets/shaders/default.glsl");
-	m_DebugShader = m_ShaderLibrary.Load("./assets/shaders/unlitPosGradient.glsl");
-	m_SelectedShader = m_DefaultShader;
-
-	m_SelectedShader->Attach();
-	m_SelectedShader->SetInt("u_Material.diffuse", 0);
-	m_SelectedShader->SetInt("u_Material.specular", 1);
-	m_SelectedShader->SetInt("u_Material.normalMap", 2);
-
-	//m_Model.GenerateUnitCube();
-	m_Model.LoadOBJFromFile("./assets/models/monkey.obj");
-	m_Sphere.LoadOBJFromFile("./assets/models/sphere.obj");
-
-	m_Plane.GenerateTerrain(10, 0);
-
-	float lightPower = 5.0f;
-	glm::vec3 lightColor = { 1.0f, 1.0f, 1.0f };
-	m_SelectedShader->SetFloat3("u_Light.position", m_PointLightPosition);
-	m_SelectedShader->SetFloat3("u_Light.ambient", lightColor * 0.005f * lightPower);
-	m_SelectedShader->SetFloat3("u_Light.diffuse", lightColor * 0.2f * lightPower);
-	m_SelectedShader->SetFloat3("u_Light.specular", lightColor * lightPower);
-	// Light will cover a distance of 50 meters
-	m_SelectedShader->SetFloat("u_Light.constant", 1.0f);
-	m_SelectedShader->SetFloat("u_Light.linear", 0.09f);
-	m_SelectedShader->SetFloat("u_Light.quadratic", 0.032f);
-
-	m_SelectedShader->SetFloat("u_Material.shininess", 512.0f);
-}
-
-void SandboxGame::OnUpdate(float elapsedTime)
-{
-	static float accumulatedTime = 0.0f;
-	accumulatedTime += elapsedTime;
-
-	if (accumulatedTime > 1.0f)
+public:
+	virtual void OnCreate() override
 	{
-		std::cout << std::fixed << 1.0f / elapsedTime << " fps | frametime: " << elapsedTime * 1000 << "ms\n";
-		accumulatedTime = 0.0f;
+		m_CameraController.GetCamera().SetPosition({ 0.0f, 1.0f, -8.0f });
 
-		/*std::cout << "Position: " << m_CameraController.GetCamera().GetPosition() << std::endl;
-		std::cout << "Velocity: " << m_CameraController.GetCamera().GetVelocity() << std::endl;
-		std::cout << "Orientation: " << m_CameraController.GetCamera().GetOrientation() << std::endl;*/
+		m_DefaultShader = m_ShaderLibrary.Load("./assets/shaders/default.glsl");
+		m_DebugShader = m_ShaderLibrary.Load("./assets/shaders/unlitPosGradient.glsl");
+		m_SelectedShader = m_DefaultShader;
+
+		m_SelectedShader->Attach();
+		m_SelectedShader->SetInt("u_Material.diffuse", 0);
+		m_SelectedShader->SetInt("u_Material.specular", 1);
+		m_SelectedShader->SetInt("u_Material.normalMap", 2);
+
+		//m_Model.GenerateUnitCube();
+		m_Model.LoadOBJFromFile("./assets/models/monkey.obj");
+		m_Sphere.LoadOBJFromFile("./assets/models/sphere.obj");
+
+		m_Plane.GenerateTerrain(10, 0);
+
+		float lightPower = 5.0f;
+		glm::vec3 lightColor = { 1.0f, 1.0f, 1.0f };
+		m_SelectedShader->SetFloat3("u_Light.position", m_PointLightPosition);
+		m_SelectedShader->SetFloat3("u_Light.ambient", lightColor * 0.005f * lightPower);
+		m_SelectedShader->SetFloat3("u_Light.diffuse", lightColor * 0.2f * lightPower);
+		m_SelectedShader->SetFloat3("u_Light.specular", lightColor * lightPower);
+		// Light will cover a distance of 50 meters
+		m_SelectedShader->SetFloat("u_Light.constant", 1.0f);
+		m_SelectedShader->SetFloat("u_Light.linear", 0.09f);
+		m_SelectedShader->SetFloat("u_Light.quadratic", 0.032f);
+
+		m_SelectedShader->SetFloat("u_Material.shininess", 512.0f);
 	}
 
-	const float speed = 10.0f;
-	if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::RightShift))
+	virtual void OnUpdate(float elapsedTime) override
 	{
-		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Up))
+		static float accumulatedTime = 0.0f;
+		accumulatedTime += elapsedTime;
+
+		if (accumulatedTime > 1.0f)
 		{
-			m_PointLightPosition.y += elapsedTime * speed;
+			std::cout << std::fixed << 1.0f / elapsedTime << " fps | frametime: " << elapsedTime * 1000 << "ms\n";
+			accumulatedTime = 0.0f;
+
+			auto& window = Application::Get().GetWindow();
+			std::cout << "Window resolution: " << window.GetWidth() << "x" << window.GetHeight() << std::endl;
+
+			/*std::cout << "Position: " << m_CameraController.GetCamera().GetPosition() << std::endl;
+			std::cout << "Velocity: " << m_CameraController.GetCamera().GetVelocity() << std::endl;
+			std::cout << "Orientation: " << m_CameraController.GetCamera().GetOrientation() << std::endl;*/
 		}
 
-		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Down))
+		const float speed = 10.0f;
+		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::RightShift))
 		{
-			m_PointLightPosition.y -= elapsedTime * speed;
+			if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Up))
+			{
+				m_PointLightPosition.y += elapsedTime * speed;
+			}
+
+			if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Down))
+			{
+				m_PointLightPosition.y -= elapsedTime * speed;
+			}
+		}
+		else
+		{
+			if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Up))
+			{
+				m_PointLightPosition.z += elapsedTime * speed;
+			}
+
+			if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Down))
+			{
+				m_PointLightPosition.z -= elapsedTime * speed;
+			}
+		}
+
+		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Right))
+		{
+			m_PointLightPosition.x += elapsedTime * speed;
+		}
+
+		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Left))
+		{
+			m_PointLightPosition.x -= elapsedTime * speed;
+		}
+
+		m_CameraController.OnUpdate(elapsedTime);
+
+		m_PortalCameraController.GetCamera().SetPosition({ 0.0f, 1.0f, -4.0f });
+		m_PortalCameraController.GetCamera().SetOrientation(0.0f, { 1.0f, 0.0f, 0.0f });
+		m_PortalCameraController.GetCamera().SetFov(45.0f);
+		m_PortalCameraController.OnUpdate(elapsedTime);
+
+		m_SelectedShader->SetFloat3("u_Light.position", m_PointLightPosition);
+
+		//m_ModelTransform = glm::translate(m_ModelTransform, elapsedTime * glm::vec3(0.0f, 0.0f, 0.0f));
+		m_ModelTransform = glm::rotate(m_ModelTransform, elapsedTime, glm::vec3(0.0f, 1.0f, 0.0f));
+
+		{
+			Entropy::Renderer::BeginScene(m_PortalCameraController.GetCamera());
+
+			m_Framebuffer->Attach();
+
+			// Drawing Suzanne to the framebuffer
+			Entropy::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
+			Entropy::RenderCommand::Clear();
+			white->Attach(0);
+			white->Attach(1);
+			Entropy::Renderer::Submit(m_SelectedShader, m_Model.GetVertexArray(), m_ModelTransform);
+
+			m_Framebuffer->Detach();
+
+			Entropy::Renderer::EndScene();
+		}
+
+		Entropy::RenderCommand::SetViewport(0, 0, Application::Get().GetWindow().GetWidth(), Application::Get().GetWindow().GetHeight());
+
+		{
+			Entropy::Renderer::BeginScene(m_CameraController.GetCamera());
+
+			white->Attach(0);
+			white->Attach(1);
+			Entropy::Renderer::Submit(m_SelectedShader, m_Model.GetVertexArray(), m_ModelTransform);
+
+			m_Framebuffer->AttachColorAttachment(0);
+			//diffuseMap->Attach(0);
+			specularMap->Attach(1);
+			Entropy::Renderer::Submit(m_SelectedShader, m_Plane.GetVertexArray(), m_PlaneTransform);
+
+			white->Attach(0);
+			white->Attach(1);
+			Entropy::Renderer::Submit(m_SelectedShader, m_Sphere.GetVertexArray(), glm::translate(glm::mat4(1.0f), m_PointLightPosition) * m_SphereTransform);
+
+			Entropy::Renderer::EndScene();
 		}
 	}
-	else
+
+	virtual void OnApplicationEvent(Entropy::Event& e) override
 	{
-		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Up))
+		Entropy::EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<Entropy::MouseButtonPressedEvent>(NT_ATTACH_EVENT_FN(SandboxGame::OnMouseButtonPressed));
+		dispatcher.Dispatch<Entropy::KeyPressedEvent>(NT_ATTACH_EVENT_FN(SandboxGame::OnKeyPressed));
+
+		m_CameraController.OnEvent(e);
+	}
+
+	bool OnMouseButtonPressed(Entropy::MouseButtonPressedEvent& e)
+	{
+		switch (e.GetMouseButton())
 		{
-			m_PointLightPosition.z += elapsedTime * speed;
-		}
-
-		if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Down))
+		case Entropy::MouseCode::Button0:
 		{
-			m_PointLightPosition.z -= elapsedTime * speed;
+			// Focus enabled
+			GetWindow().Select();
+			break;
 		}
+		}
+		return false;
 	}
 
-	if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Right))
+	bool OnKeyPressed(Entropy::KeyPressedEvent& e)
 	{
-		m_PointLightPosition.x += elapsedTime * speed;
+		switch (e.GetKeyCode())
+		{
+		case Entropy::KeyCode::F1:
+		{
+			// Toggle debug shader
+			static bool debugShaderEnabled = false;
+			debugShaderEnabled = !debugShaderEnabled;
+			m_SelectedShader = debugShaderEnabled ? m_DebugShader : m_DefaultShader;
+
+			std::stringstream ss;
+			ss << "Shader used: ";
+			ss << m_SelectedShader->GetName();
+			NT_TRACE(ss.str());
+			break;
+		}
+		case Entropy::KeyCode::F3:
+		{
+			// Toggle VSync
+			auto& window = GetWindow();
+			window.SetVSync(!window.IsVSync());
+
+			std::stringstream ss;
+			ss << "VSync is now ";
+			ss << (window.IsVSync() ? "enabled" : "disabled");
+			NT_TRACE(ss.str());
+			break;
+		}
+		case Entropy::KeyCode::F11:
+		{
+			GetWindow().EnableFullScreen();
+			break;
+		}
+		case Entropy::KeyCode::Escape:
+		{
+			// Focus lost
+			GetWindow().Deselect();
+			break;
+		}
+		}
+		return false;
 	}
 
-	if (Entropy::Input::IsKeyPressed(Entropy::KeyCode::Left))
-	{
-		m_PointLightPosition.x -= elapsedTime * speed;
-	}
+private:
+	Entropy::Ref<Entropy::Texture2D> diffuseMap = Entropy::Texture2D::Create("./assets/textures/container.png");
+	Entropy::Ref<Entropy::Texture2D> specularMap = Entropy::Texture2D::Create("./assets/textures/container_specular.png");
+	Entropy::Ref<Entropy::Texture2D> normalMap = Entropy::Texture2D::Create("./assets/textures/normal_map.png");
+	Entropy::Ref<Entropy::Texture2D> white = Entropy::Texture2D::Create("./assets/textures/white.jpg");
+	Entropy::Ref<Entropy::Texture2D> cobblestone = Entropy::Texture2D::Create("./assets/textures/cobblestone.png");
 
-	m_CameraController.OnUpdate(elapsedTime);
+	Entropy::ShaderLibrary m_ShaderLibrary;
+	Entropy::Ref<Entropy::Shader> m_DefaultShader;
+	Entropy::Ref<Entropy::Shader> m_DebugShader;
+	Entropy::Ref<Entropy::Shader> m_SelectedShader;
 
-	m_PortalCameraController.GetCamera().SetPosition({ 0.0f, 1.0f, -4.0f });
-	m_PortalCameraController.GetCamera().SetOrientation(0.0f, { 1.0f, 0.0f, 0.0f });
-	m_PortalCameraController.GetCamera().SetFov(45.0f);
-	m_PortalCameraController.OnUpdate(elapsedTime);
+	Entropy::Mesh m_Plane;
+	Entropy::Mesh m_Model;
+	Entropy::Mesh m_Sphere;
 
-	m_SelectedShader->SetFloat3("u_Light.position", m_PointLightPosition);
+	glm::mat4 m_PlaneTransform = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+	glm::mat4 m_SphereTransform = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(0.01f, 0.01f, 0.01f));
+	glm::mat4 m_ModelTransform = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 1.0f, 0.0f)) * glm::scale(glm::mat4(1.0f), glm::vec3(1.0f, 1.0f, 1.0f));
 
-	//m_ModelTransform = glm::translate(m_ModelTransform, elapsedTime * glm::vec3(0.0f, 0.0f, 0.0f));
-	m_ModelTransform = glm::rotate(m_ModelTransform, elapsedTime, glm::vec3(0.0f, 1.0f, 0.0f));
+	glm::vec3 m_PointLightPosition = glm::vec3(-0.7f, 2.0f, -5.0f);
 
-	{
-		Entropy::Renderer::BeginScene(m_PortalCameraController.GetCamera());
+	Entropy::CameraController m_CameraController = Entropy::CameraController((float)GetWindow().GetWidth() / (float)GetWindow().GetHeight());
 
-		m_Framebuffer->Attach();
+	Entropy::Ref<Entropy::Framebuffer> m_Framebuffer = Entropy::Framebuffer::Create(2048, 2048);
+	Entropy::CameraController m_PortalCameraController = Entropy::CameraController(1.0f);
+};
 
-		// Drawing Suzanne to the framebuffer
-		Entropy::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1.0f });
-		Entropy::RenderCommand::Clear();
-		white->Attach(0);
-		white->Attach(1);
-		Entropy::Renderer::Submit(m_SelectedShader, m_Model.GetVertexArray(), m_ModelTransform);
-
-		m_Framebuffer->Detach();
-
-		Entropy::Renderer::EndScene();
-	}
-		
-	Entropy::RenderCommand::SetViewport(0, 0, Application::Get().GetWindow().GetWidth(), Application::Get().GetWindow().GetHeight());
-
-	{
-		Entropy::Renderer::BeginScene(m_CameraController.GetCamera());
-
-		white->Attach(0);
-		white->Attach(1);
-		Entropy::Renderer::Submit(m_SelectedShader, m_Model.GetVertexArray(), m_ModelTransform);
-
-		m_Framebuffer->AttachColorAttachment(0);
-		//diffuseMap->Attach(0);
-		specularMap->Attach(1);
-		Entropy::Renderer::Submit(m_SelectedShader, m_Plane.GetVertexArray(), m_PlaneTransform);
-
-		white->Attach(0);
-		white->Attach(1);
-		Entropy::Renderer::Submit(m_SelectedShader, m_Sphere.GetVertexArray(), glm::translate(glm::mat4(1.0f), m_PointLightPosition) * m_SphereTransform);
-
-		Entropy::Renderer::EndScene();
-	}
-}
-
-void SandboxGame::OnApplicationEvent(Entropy::Event& e)
+Entropy::Application* Entropy::CreateApplication()
 {
-	Entropy::EventDispatcher dispatcher(e);
-	dispatcher.Dispatch<Entropy::MouseButtonPressedEvent>(NT_ATTACH_EVENT_FN(SandboxGame::OnMouseButtonPressed));
-	dispatcher.Dispatch<Entropy::KeyPressedEvent>(NT_ATTACH_EVENT_FN(SandboxGame::OnKeyPressed));
-
-	m_CameraController.OnEvent(e);
-}
-
-bool SandboxGame::OnMouseButtonPressed(Entropy::MouseButtonPressedEvent& e)
-{
-	switch (e.GetMouseButton())
-	{
-	case Entropy::MouseCode::Button0:
-	{
-		// Focus enabled
-		GetWindow().Select();
-		break;
-	}
-	}
-	return false;
-}
-
-bool SandboxGame::OnKeyPressed(Entropy::KeyPressedEvent& e)
-{
-	switch (e.GetKeyCode())
-	{
-	case Entropy::KeyCode::F1:
-	{
-		// Toggle debug shader
-		static bool debugShaderEnabled = false;
-		debugShaderEnabled = !debugShaderEnabled;
-		m_SelectedShader = debugShaderEnabled ? m_DebugShader : m_DefaultShader;
-
-		std::stringstream ss;
-		ss << "Shader used: ";
-		ss << m_SelectedShader->GetName();
-		NT_TRACE(ss.str());
-		break;
-	}
-	case Entropy::KeyCode::F3:
-	{
-		// Toggle VSync
-		auto& window = GetWindow();
-		window.SetVSync(!window.IsVSync());
-
-		std::stringstream ss;
-		ss << "VSync is now ";
-		ss << (window.IsVSync() ? "enabled" : "disabled");
-		NT_TRACE(ss.str());
-		break;
-	}
-	case Entropy::KeyCode::F11:
-	{
-		auto& window = GetWindow();
-		window.EnableFullScreen();
-		break;
-	}
-	case Entropy::KeyCode::Escape:
-	{
-		// Focus lost
-		GetWindow().Deselect();
-		break;
-	}
-	}
-	return false;
+	return new SandboxGame();
 }
