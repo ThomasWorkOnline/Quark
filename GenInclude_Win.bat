bd8ef5dace9cbd5233506174d29369758ac1f247
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2021-11-18 23:04:14 -0500

    Update GenInclude_Win.bat

--- GenInclude_Win.bat
+++ GenInclude_Win.bat
@@ -3,10 +3,10 @@ set version=1.0
 set dest=quark-v%version%
 
 :: Copy Quark header files
-xcopy /e .\Quark\src\*.h .\%dest%\include\QK\
+xcopy /e .\Quark\src\*.h .\%dest%\include\
 
 :: Copy binaries
-xcopy .\bin\Quark\x64-Debug\*.lib .\%dest%\lib\Windows\x64-Debug\
-xcopy .\bin\Quark\x64-Release\*.lib .\%dest%\lib\Windows\x64-Release\
+xcopy .\bin\windows-Debug-x86_64\Quark\*.lib .\%dest%\lib\Windows-x64-Debug\
+xcopy .\bin\windows-Release-x86_64\Quark\*.lib .\%dest%\lib\Windows-x64-Release\
 
 pause
