d0a392a81474c861a7ad49a1049dfaac5bdb9053
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2021-11-23 17:14:00 -0500

    Remove virtual specifiers for camera controllers

--- Quark/src/Quark/Input/PerspectiveCameraController.h
+++ Quark/src/Quark/Input/PerspectiveCameraController.h
@@ -15,23 +15,20 @@ namespace Quark {
 	public:
 		PerspectiveCameraController() = default;
 		PerspectiveCameraController(Entity camera);
-		virtual ~PerspectiveCameraController() = default;
 
 		/// <summary>
 		/// Propagates the events to the controller.
 		/// Make sure the camera entity has all `Transform3DComponent`, `PhysicsComponent` and `PerspectiveCameraComponent` components.
-		/// This method can be overriden.
 		/// </summary>
 		/// <param name="e">Event to propagate</param>
-		virtual void OnEvent(Event& e);
+		void OnEvent(Event& e);
 
 		/// <summary>
 		/// Updates the logic for controlling the bound camera.
 		/// Make sure the camera entity has all `Transform3DComponent`, `PhysicsComponent` and `PerspectiveCameraComponent` components.
-		/// This method can be overriden.
 		/// </summary>
 		/// <param name="elapsedTime">Time elapsed between the last update</param>
-		virtual void OnUpdate(float elapsedTime);
+		void OnUpdate(float elapsedTime);
 
 		bool HasCamera() const { return m_CameraEntity; }
 		void AttachCamera(Entity camera) { m_CameraEntity = camera; }
