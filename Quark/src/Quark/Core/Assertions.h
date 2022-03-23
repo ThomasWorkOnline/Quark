3fcbb434e785d811db1dd9767e41e091dc94ef0e
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2022-03-23 01:50:08 -0400

    fix tab formatting for spaces

--- Quark/src/Quark/Core/Assertions.h
+++ Quark/src/Quark/Core/Assertions.h
@@ -4,32 +4,32 @@
 #include "Logger.h"
 
 #ifdef QK_DEBUG
-#	define QK_ENABLE_ASSERTIONS
+#   define QK_ENABLE_ASSERTIONS
 #endif
 
 // Debugbreak
 #ifdef QK_ENABLE_ASSERTIONS
-	#if defined(QK_PLATFORM_WINDOWS)
-	#	define QK_DEBUGBREAK() __debugbreak()
-	#elif defined(QK_PLATFORM_LINUX)
-	#	include <signal.h>
-	#	define QK_DEBUGBREAK() raise(SIGTRAP)
-	#elif defined(QK_PLATFORM_APPLE)
-	#	define QK_DEBUGBREAK() abort()
-	#else
-		// Missing debugbreak implementation for platform
-	#	define QK_DEBUGBREAK()
-	#endif
+   #if defined(QK_PLATFORM_WINDOWS)
+   #   define QK_DEBUGBREAK() __debugbreak()
+   #elif defined(QK_PLATFORM_LINUX)
+   #   include <signal.h>
+   #   define QK_DEBUGBREAK() raise(SIGTRAP)
+   #elif defined(QK_PLATFORM_APPLE)
+   #   define QK_DEBUGBREAK() abort()
+   #else
+   	// Missing debugbreak implementation for platform
+   #   define QK_DEBUGBREAK()
+   #endif
 #else
-#	define QK_DEBUGBREAK()
+#   define QK_DEBUGBREAK()
 #endif
 
 #ifdef QK_ENABLE_ASSERTIONS
-#	define QK_CORE_ASSERT(x, ...) do { if(!(x)) { QK_CORE_ERROR(__VA_ARGS__); QK_DEBUGBREAK(); } } while (false)
-#	define QK_ASSERT(x, ...) do { if(!(x)) { QK_ERROR(__VA_ARGS__); QK_DEBUGBREAK(); } } while (false)
+#   define QK_CORE_ASSERT(x, ...) do { if(!(x)) { QK_CORE_ERROR(__VA_ARGS__); QK_DEBUGBREAK(); } } while (false)
+#   define QK_ASSERT(x, ...) do { if(!(x)) { QK_ERROR(__VA_ARGS__); QK_DEBUGBREAK(); } } while (false)
 #else
-#	define QK_CORE_ASSERT(x, ...)
-#	define QK_ASSERT(x, ...)
+#   define QK_CORE_ASSERT(x, ...)
+#   define QK_ASSERT(x, ...)
 #endif
 
 #define QK_FATAL(...) do { QK_CORE_ERROR(__VA_ARGS__); QK_DEBUGBREAK(); } while (false)
--- Quark/src/Quark/Core/Logger.h
+++ Quark/src/Quark/Core/Logger.h
@@ -6,25 +6,25 @@
 
 // Use unbuffered std::cerr for errors
 #ifdef QK_DEBUG
-#	define QK_CORE_TRACE(...) std::cout << "[Core Trace]: "		<< __VA_ARGS__ << std::endl
-#	define QK_CORE_INFO(...)  std::cout << "[Core Info]: "		<< __VA_ARGS__ << std::endl
-#	define QK_CORE_WARN(...)  std::cout << "[Core Warning]: "	<< __VA_ARGS__ << std::endl
-#	define QK_CORE_ERROR(...) std::cerr << "[CORE ERROR]: "		<< __VA_ARGS__ << std::endl
-
-#	define QK_TRACE(...) std::cout << "[Trace]: "		        << __VA_ARGS__ << std::endl
-#	define QK_INFO(...)  std::cout << "[Info]: "		        << __VA_ARGS__ << std::endl
-#	define QK_WARN(...)  std::cout << "[Warning]: "	            << __VA_ARGS__ << std::endl
-#	define QK_ERROR(...) std::cerr << "[ERROR]: "		        << __VA_ARGS__ << std::endl
+#   define QK_CORE_TRACE(...) std::cout << "[Core Trace]: "     << __VA_ARGS__ << std::endl
+#   define QK_CORE_INFO(...)  std::cout << "[Core Info]: "      << __VA_ARGS__ << std::endl
+#   define QK_CORE_WARN(...)  std::cout << "[Core Warning]: "   << __VA_ARGS__ << std::endl
+#   define QK_CORE_ERROR(...) std::cerr << "[CORE ERROR]: "     << __VA_ARGS__ << std::endl
+    
+#   define QK_TRACE(...) std::cout << "[Trace]: "               << __VA_ARGS__ << std::endl
+#   define QK_INFO(...)  std::cout << "[Info]: "                << __VA_ARGS__ << std::endl
+#   define QK_WARN(...)  std::cout << "[Warning]: "             << __VA_ARGS__ << std::endl
+#   define QK_ERROR(...) std::cerr << "[ERROR]: "		        << __VA_ARGS__ << std::endl
 #else
-#	define QK_CORE_TRACE(...)                                   // Tracing disabled
-#	define QK_CORE_INFO(...)  std::cout << "[Core Info]: "		<< __VA_ARGS__ << std::endl
-#	define QK_CORE_WARN(...)  std::cout << "[Core Warning]: "	<< __VA_ARGS__ << std::endl
-#	define QK_CORE_ERROR(...) std::cerr << "[CORE ERROR]: "		<< __VA_ARGS__ << std::endl
-
-#	define QK_TRACE(...)                                        // Tracing disabled
-#	define QK_INFO(...)  std::cout << "[Info]: "		        << __VA_ARGS__ << std::endl
-#	define QK_WARN(...)  std::cout << "[Warning]: "	            << __VA_ARGS__ << std::endl
-#	define QK_ERROR(...) std::cerr << "[ERROR]: "		        << __VA_ARGS__ << std::endl
+#   define QK_CORE_TRACE(...)                                   // Tracing disabled
+#   define QK_CORE_INFO(...)  std::cout << "[Core Info]: "      << __VA_ARGS__ << std::endl
+#   define QK_CORE_WARN(...)  std::cout << "[Core Warning]: "   << __VA_ARGS__ << std::endl
+#   define QK_CORE_ERROR(...) std::cerr << "[CORE ERROR]: "     << __VA_ARGS__ << std::endl
+    
+#   define QK_TRACE(...)                                        // Tracing disabled
+#   define QK_INFO(...)  std::cout << "[Info]: "                << __VA_ARGS__ << std::endl
+#   define QK_WARN(...)  std::cout << "[Warning]: "             << __VA_ARGS__ << std::endl
+#   define QK_ERROR(...) std::cerr << "[ERROR]: "               << __VA_ARGS__ << std::endl
 #endif
 
 #include "LogUtils.h"
