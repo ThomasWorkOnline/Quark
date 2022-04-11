da479171afaa8ba8f1cd2b6734565d9a30e07abe
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2022-04-11 01:31:28 -0400

    Update premake5.lua

--- Quark/vendor/freetype/premake5.lua
+++ Quark/vendor/freetype/premake5.lua
@@ -56,7 +56,6 @@ project "freetype"
 		"src/sdf/sdf.c",
 		"src/sfnt/sfnt.c",
 		"src/smooth/smooth.c",
-		"src/smooth/ftgrays.c",
 		"src/svg/svg.c",
 		"src/truetype/truetype.c",
 		"src/type1/type1.c",
