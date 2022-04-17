53a968cbd20abfd0de4d42a75d84d1fe456483c9
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2022-04-17 05:29:41 -0400

    upload spash

--- Demos/src/Main.cpp
+++ Demos/src/Main.cpp
@@ -1,10 +1,10 @@
-#include "Example1_BasicRendering.h"
+#include "Tests/PBRRendering.h"
 
 namespace Quark {
 
 	int Main(int argc, char** argv)
 	{
-		auto test = CreateScope<YourApplication>();
+		auto test = CreateScope<PBRRendering>();
 		test->Run();
 
 		return 0;
