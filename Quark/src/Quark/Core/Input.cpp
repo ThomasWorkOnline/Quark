18d95adcfa7ddea6e897e47530282d9dd4d03222
ThomasWorkOnline <48111213+ThomasWorkOnline@users.noreply.github.com>
2022-06-30 21:30:05 -0400

    remove redundant GLFW_REPEAT in IsKeyPressed

--- Quark/src/Quark/Core/Input.cpp
+++ Quark/src/Quark/Core/Input.cpp
@@ -10,15 +10,13 @@ namespace Quark {
 	bool Input::IsKeyPressed(KeyCode key)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-		auto state = glfwGetKey(window, static_cast<int32_t>(key));
-		return state == GLFW_PRESS || state == GLFW_REPEAT;
+		return glfwGetKey(window, static_cast<int32_t>(key)) == GLFW_PRESS;
 	}
 
 	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
-		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
-		return state == GLFW_PRESS;
+		return glfwGetMouseButton(window, static_cast<int32_t>(button)) == GLFW_PRESS;
 	}
 
 	std::pair<float, float> Input::GetMousePosition()
