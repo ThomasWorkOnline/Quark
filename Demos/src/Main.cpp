a7fcd6ff7e7487db18a3783dcf6ff077e42d8415
Thomas Lessard <thomaslessard122@hotmail.com>
2022-08-16 22:47:59 -0400

    Setup Python script to automate build process and install dependencies

--- Demos/src/Main.cpp
+++ Demos/src/Main.cpp
@@ -1,9 +1,7 @@
 #include <Quark/EntryPoint.h>
 
 #include "Demos/PBRRenderingDemo.h"
-
 #include "FontTest/FontTest.h"
-
 #include "Example1_BasicRendering.h"
 #include "TextureArrayTest.h"
 #include "VulkanApp.h"
--- Setup.py
+++ Setup.py
@@ -1,5 +1,14 @@
 import os
+import platform
 import subprocess
 
-print("Installing submodules...")
-subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
+from Scripts.VulkanConfiguraton import VulkanConfiguration as VulkanRequirements
+
+VulkanRequirements.Validate()
+
+if platform.system() == "Windows":
+	dirname = os.path.dirname(__file__)
+	os.chdir(dirname) # Run subprocess as if we are in the solution root
+
+	print("Generating Visual Studio solution using Premake...")
+	subprocess.call([os.path.abspath("./Scripts/Windows/GenVisualStudio2022.bat")])
