8a29367652a4b75695926c45f42ed27a45c6a421
Thomas Lessard <thomaslessard122@hotmail.com>
2022-08-27 03:32:08 -0400

    Removed ReadByteCode obsolete function

--- Quark/src/Platform/Vulkan/VulkanShader.cpp
+++ Quark/src/Platform/Vulkan/VulkanShader.cpp
@@ -8,21 +8,6 @@ namespace Quark {
 
 	namespace Utils {
 
-		static std::vector<char> ReadByteCode(std::string_view filename)
-		{
-			std::ifstream in(filename.data(), std::ios::ate | std::ios::binary);
-			QK_CORE_ASSERT(in.is_open(), "Failed to open file");
-
-			size_t fileSize = (size_t)in.tellg();
-			std::vector<char> buffer(fileSize);
-
-			in.seekg(0);
-			in.read(buffer.data(), fileSize);
-			in.close();
-
-			return buffer;
-		}
-
 		static constexpr VkShaderStageFlagBits GetShaderStageType(std::string_view filepath)
 		{
 			VkShaderStageFlagBits stage{};
