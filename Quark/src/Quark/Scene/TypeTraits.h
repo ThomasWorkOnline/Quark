7105c1480c6585d5de2742aa97a18b6ad2355e70
Thomas Lessard <thomaslessard122@hotmail.com>
2022-10-03 15:52:28 -0400

    Simplified TypeTraits drastically

--- Quark/src/Quark/Scene/TypeTraits.h
+++ Quark/src/Quark/Scene/TypeTraits.h
@@ -9,55 +9,43 @@ namespace Quark {
 	class HasOnCreate
 	{
 	private:
-		template <typename C>
-		static constexpr std::true_type Test(decltype(&C::OnCreate));
-
-		template <typename C>
+		static constexpr std::true_type  Test(decltype(&T::OnCreate));
 		static constexpr std::false_type Test(...);
 
 	public:
-		static constexpr bool value = std::is_same_v<decltype(Test<T>(0)), std::true_type>;
+		static constexpr bool value = decltype(Test(0))::value;
 	};
 
 	template <typename T>
 	class HasOnDestroy
 	{
 	private:
-		template <typename C>
-		static constexpr std::true_type Test(decltype(&C::OnDestroy));
-
-		template <typename C>
+		static constexpr std::true_type  Test(decltype(&T::OnDestroy));
 		static constexpr std::false_type Test(...);
 
 	public:
-		static constexpr bool value = std::is_same_v<decltype(Test<T>(0)), std::true_type>;
+		static constexpr bool value = decltype(Test(0))::value;
 	};
 
 	template <typename T>
 	class HasOnUpdate
 	{
 	private:
-		template <typename C>
-		static constexpr std::true_type Test(decltype(&C::OnUpdate));
-
-		template <typename C>
+		static constexpr std::true_type  Test(decltype(&T::OnUpdate));
 		static constexpr std::false_type Test(...);
 
 	public:
-		static constexpr bool value = std::is_same_v<decltype(Test<T>(0)), std::true_type>;
+		static constexpr bool value = decltype(Test(0))::value;
 	};
 
 	template <typename T>
 	class HasOnEvent
 	{
 	private:
-		template <typename C>
-		static constexpr std::true_type Test(decltype(&C::OnEvent));
-
-		template <typename C>
+		static constexpr std::true_type  Test(decltype(&T::OnEvent));
 		static constexpr std::false_type Test(...);
 
 	public:
-		static constexpr bool value = std::is_same_v<decltype(Test<T>(0)), std::true_type>;
+		static constexpr bool value = decltype(Test(0))::value;
 	};
 }
