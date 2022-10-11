a6893ebe59f59553562ec82e7b0933fa4d6f97ac
Thomas Lessard <thomaslessard122@hotmail.com>
2022-10-10 20:50:15 -0400

    Fix Python script when building solution from outside the root folder

--- Setup.py
+++ Setup.py
@@ -7,11 +7,11 @@ import subprocess
 
 from Scripts.VulkanConfiguration import VulkanConfiguration as VulkanRequirements
 
+dirname = os.path.dirname(__file__)
+os.chdir(dirname) # Run subprocess as if we are in the root folder
+
 VulkanRequirements.Validate()
 
 if platform.system() == "Windows":
-	dirname = os.path.dirname(__file__)
-	os.chdir(dirname) # Run subprocess as if we are in the root folder
-
 	print("Generating Visual Studio solution using Premake...")
 	subprocess.call([os.path.abspath("./Scripts/Windows/GenVisualStudio2022.bat")])
